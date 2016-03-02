c4ab2b0842cb4c25f082eaf85c31c622d7b77978
przemek <przempore@gmail.com>
2016-03-02 12:57:33 +0100

    #1. Customize CMakeLists.txt to Windows | build.bat improved

--- build.bat
+++ build.bat
@@ -15,10 +15,10 @@ if "%1" == "-d" (
 if "%1" == "-r" (
 	if %DeadstormBuilded%  == 1 (
 		if not exist bin\game\Debug\SDL2.dll (
-			copy %SDL2%\lib\x86\SDL2.dll C:\Users\przemo\Repository\Deadstorm\bin\game\Debug\
+			copy %SDL2%\lib\x86\SDL2.dll bin\game\Debug\
 			)
-		if not exist C:\Users\przemo\Repository\Deadstorm\bin\game\Debug\SDL2_image.dll (
-			copy %SDL2_image%\lib\x86\SDL2_image.dll C:\Users\przemo\Repository\Deadstorm\bin\game\Debug\
+		if not exist bin\game\Debug\SDL2_image.dll (
+			copy %SDL2_image%\lib\x86\SDL2_image.dll bin\game\Debug\
 			)
 		bin\game\Debug\Deadstorm.exe
 		goto eof
