b269e9773a8a364c578f3bfbcfc9c50d66ba343f
przemek <przempore@gmail.com>
2016-03-02 12:09:31 +0100

    #1. Customize CMakeLists.txt to Windows | it works

--- build.bat
+++ build.bat
@@ -1,6 +1,13 @@
 @echo off
 
-if not exist bin mkdir bin
-cd bin
-cmake ..
-msbuild Deadstorm.sln
+if "%1" == "-b" (
+  if not exist bin mkdir bin
+  cd bin
+  cmake ..
+  cd ..
+  msbuild bin/Deadstorm.sln
+	)
+
+if "%1" == "-d" (
+	rmdir /s /q bin
+	)
